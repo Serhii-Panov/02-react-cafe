4a1aef32850283bac7bb929fb5bcb8e338d4f012
Serhii Panov <suboxoneforhelp@gmail.com>
2025-11-27 11:43:34 +0200

    step 2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,9 @@
 import CafeInfo from "../CafeInfo/CafeInfo.tsx"
 import css from './App.module.css'
 import { useState } from "react";
-import Votes from "../../types/votes.ts";
+
 function App() {
-  const [votes:Votes, setVotes] = {
-    good: 0,
-    neutral: 0,
-    bad:0
-  }
-  function handleClick() {
-    setVotes()
-  }
+
   return (<div className={css.app}><CafeInfo /></div>);
 }
 
