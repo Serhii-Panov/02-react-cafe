d17b68519049766a713a4c91e07fc4caa0ca7c23
Serhii Panov <suboxoneforhelp@gmail.com>
2025-11-27 12:27:48 +0200

    step 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,24 @@
 import CafeInfo from "../CafeInfo/CafeInfo.tsx"
 import css from './App.module.css'
-
+import { useState } from "react"
+import type Votes from "../../types/votes.ts"
+import type { VoteType } from "../../types/votes.ts"
 function App() {
-
+    const initialState: Votes = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  const [votes, setVotes] = useState<Votes>(initialState);
+  const handleVoteType = (type: VoteType) => {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  }
+    const resetVotes = () => {
+    setVotes(initialState);
+  };
   return (<div className={css.app}><CafeInfo /></div>);
 }
 
