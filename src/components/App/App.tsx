ffbaa430710766e85357ba5c89d17fbeb7d3ee99
Serhii Panov <suboxoneforhelp@gmail.com>
2025-11-27 13:06:08 +0200

    step 4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,31 @@
-import CafeInfo from "../CafeInfo/CafeInfo.tsx"
-import css from './App.module.css'
-import { useState } from "react"
-import type Votes from "../../types/votes.ts"
-import type { VoteType } from "../../types/votes.ts"
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import css from "./App.module.css";
+import { useState } from "react";
+import type Votes from "../../types/votes.ts";
+import type { VoteType } from "../../types/votes.ts";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 function App() {
-    const initialState: Votes = {
+  const initialState: Votes = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
   const [votes, setVotes] = useState<Votes>(initialState);
   const handleVoteType = (type: VoteType) => {
-    setVotes(prevVotes => ({
-      ...prevVotes,
-      [type]: prevVotes[type] + 1,
+    setVotes((votes) => ({
+      ...votes,
+      [type]: votes[type] + 1,
     }));
-  }
-    const resetVotes = () => {
+  };
+  const resetVotes = () => {
     setVotes(initialState);
   };
-  return (<div className={css.app}><CafeInfo /></div>);
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVoteType} onReset={resetVotes} canReset />
+    </div>
+  );
 }
 
-export default App
+export default App;
